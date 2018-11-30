6d11ad9d1aff2f422f88fdd050bfefb0895e5966
Anwar Montasir <anwarmontasir@gmail.com>
2018-11-29 20:23:20 -0800

    interactivity

--- src/components/list.js
+++ src/components/list.js
@@ -1,16 +1,39 @@
 import React from 'react';
 
 import Card from './card';
+import AddForm from './add-form';
 
 import './list.css';
 
-export default function List() {
-    return (
-        <div className="list">
-            <h3>Example list</h3>
-            <Card />
-            <Card />
-            <Card />
-        </div>
-    );
+export default class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: [{
+                text: 'Example card 1'
+            }, {
+                text: 'Example card 2'
+            }, {
+                text: 'Example card 3'
+            }]
+        }
+    }
+    render() {
+        const cards = this.state.cards.map((card, index) => 
+            <li key={index}>
+                <Card {...card} />
+            </li>
+        );
+        return (
+            <div>
+                <h3>{this.props.title}</h3>
+                <ul className="list">
+                    {cards}
+                    <li>
+                        <AddForm type="card" />
+                    </li>
+                </ul>
+            </div>
+        );
+    }
 }
